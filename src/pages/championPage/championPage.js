e69b08a112ca601e4507093b857c60c29952be76
Sacks-Lucas <sackslm0@gmail.com>
2021-06-16 17:47:32 -0300

    Creacion lote de prueba.Champion Page vacio.Routeo

---
+++ src/pages/championPage/championPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+
+const ChampionPage = props => {
+    return (
+        <div>
+            ChampionPage
+        </div>
+    )
+}
+
+// ChampionPage.propTypes = {
+
+// }
+
+export default ChampionPage
---
+++ src/pages/championPage/championPageContainer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+import ChampionPage from './championPage'
+
+const ChampionPageContainer = props => {
+    return (
+        <div>
+            <ChampionPage/>
+        </div>
+    )
+}
+
+// ChampionPageContainer.propTypes = {
+
+// }
+
+export default ChampionPageContainer
